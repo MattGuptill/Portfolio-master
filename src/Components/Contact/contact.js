857c0879a80c6de2a5d59af7b8982ca12decb816
Matthew Guptill <matthewdguptill7@gmail.com>
2024-01-10 12:56:15 -0600

    contact changes

--- src/Components/Contact/contact.js
+++ src/Components/Contact/contact.js
@@ -5,8 +5,7 @@ export const Contact = () => {
         <div>
             <div className='contact'>
                 <h1>Contact Me:</h1>
-                <h2>Email: <a href='mailto:mattguptill@gmail.com'>mattguptill@gmail.com</a></h2>
-                <h2>Phone: 615-955-8106</h2>
+                <h2>Email: <a href='mailto:matthewdguptill7@gmail.com'>matthewdguptill7@gmail.com</a></h2>
             </div>
         </div>
     )
