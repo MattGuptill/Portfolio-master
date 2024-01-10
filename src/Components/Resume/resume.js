70933040ba971762b1acc6eb822a81f624d38a5e
Matthew Guptill <matthewdguptill7@gmail.com>
2024-01-10 13:04:44 -0600

    added new resume

--- src/Components/Resume/resume.js
+++ src/Components/Resume/resume.js
@@ -1,4 +1,4 @@
-import resume1 from '../../Resources/Photos/Frontend Developer Resume-page-001.jpg';
+import resume1 from '../../Resources/Photos/Resume.jpg';
 import resume2 from '../../Resources/Photos/Frontend Developer Resume-page-002.jpg'
 import certificate from '../../Resources/Photos/codecademy-certificate.jpg';
 
@@ -10,11 +10,6 @@ export const Resume = () => {
         <img 
         className='img' 
         src={resume1} 
-        alt='resume'
-        />
-        <img 
-        className='img'
-        src={resume2}
         alt='resume'
         />
         <img className='img' src={certificate} 
